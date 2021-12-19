84edf54fcb7ef6ab55cbc080a0de939c4612f54e
Ashton <90576442+agstatz@users.noreply.github.com>
2021-12-19 15:35:46 -0600

    Reworked application to use electron with react

---
+++ react-electron/public/main.js
@@ -0,0 +1,27 @@
+const { BrowserWindow, app } = require('electron');
+
+require('@electron/remote/main').initialize();
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            enableRemoteModule: true
+        }
+    })
+
+    win.loadURL('http://localhost:3000');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
