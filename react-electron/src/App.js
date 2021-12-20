ca31c714d2a50c96d9e05fa73303fab5ff14e156
Ashton <90576442+agstatz@users.noreply.github.com>
2021-12-19 19:54:34 -0600

    Fine tuned display features

--- react-electron/src/App.js
+++ react-electron/src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
 import React from "react";
-import { SearchPanel, SearchPanelVariant } from "react-search-panel";
-import { useState } from "react";
+import { SearchPanel, SearchPanelChoice, SearchPanelVariant } from "react-search-panel";
+import { useEffect, useState } from "react";
+
+const MIN_INPUT = 3;
 
 function App() {
   const choices = React.useState("");
   const [input, setInput] = React.useState("");
   const [selectedChoices, setSelectedChoices] = useState(choices);
-  
 
+  const handleSearchChange = (event: React.ChangeEvent) => {
+      const target = event.target;
+      setInput(target.value);
+  };
+
+  /*useEffect(() => {
+    const search = async () => {
+      const resultChoices: Array<SearchPanelChoice> = [];
+
+      // Only perform a search if end user has typed a minimum number of characters
+      if (input.length >= MIN_INPUT) {
+        const url = `${baseUrl}${input}`;
+        const response = await axios(url);
+        const results = await response.data;
+
+        // Transform results to choices.
+        results.forEach((result: ShowContainer) => {
+          const choice = { key: result.show.id.toString(), description: result.show.name };
+          resultChoices.push(choice);
+        });
+      }
+      setChoices(resultChoices);
+      setIsLoading(false);
+    };
+    search();
+  }, [input]);*/
 
   return (
     <div className="App">
@@ -16,11 +43,13 @@ function App() {
         <div class="basic-container">
             <div>
                 <h1>grocer</h1>
-                <p>Search meals, order ingredients fast and simple.</p>
+                <p class="no-click">Search meals, order ingredients fast and simple.</p>
             </div>
             <div>
                 <SearchPanel 
                     choices={choices}
+                    chips
+                    maximumHeight={250}
                     onChange={event => setInput(event.target.value)}
                     onSelectionChange={setSelectedChoices}
                     placeholder="Search"
