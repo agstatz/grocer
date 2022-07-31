01362d7b9242a91f5d7adf6df2b20336399bbd20
Ashton <90576442+agstatz@users.noreply.github.com>
2022-07-31 11:55:04 -0400

    Updated Printable Grocery List

--- react-electron/src/components/PrintableList.js
+++ react-electron/src/components/PrintableList.js
@@ -3,69 +3,126 @@
  * Sorts list of ingredients to a grocery list
  * separated by the department the item would
  * be found in
- * 
- * @date 7/28/2022
+ *
+ * @date 7/31/2022
  * @author Ashton Statz
  */
 
 import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
- 
- const PrintableList = ({ingredients}) => {
+import { Grid } from 'semantic-ui-react';
 
+const PrintableList = ({ ingredients }) => {
     const [categorizedList, setCategorizedList] = useState([]);
 
     useEffect(() => {
         const getIngredientCategories = async () => {
-            const response = await axios.get("./ingredientCategories.json");
+            const response = await axios.get('./ingredientCategories.json');
             setCategorizedList(categorize(ingredients, response.data));
-        }
+        };
         getIngredientCategories();
+    }, []);
 
+    // initializeCatList(categories)
+    // creates a base categorized list for categorize()
+    // to populate with grocery items using the categories
+    // from ingredientCategories.json
+    const initializeCatList = (categories) => {
+        let outputList = [];
 
-    }, [])
+        for (var i = 0; i < categories.length; i++) {
+            outputList.push({
+                category: categories[i].CATEGORY,
+                items: [],
+            });
+        }
+
+        return outputList;
+    };
 
     // categorize(list, categories)
     // returns a list categorized by the categories passed
     // in the second parameter.
     const categorize = (list, categories) => {
-        let outputList = [];
-        
-        for (var j = 0; j < categories.length; j++) {
-            let currentCategoryName = categories[j].CATEGORY;
-            for (var k = 0; k < categories[j].INGREDIENTS.length; k++) {
-                for (var i = 0; i < list.length; i++) {
-                    let currentIngredient = list[i].ingredient;
-                    console.log(categories[j].INGREDIENTS[k]);
-                    if (categories[j].INGREDIENTS[k] === currentIngredient) {
-                        outputList.push({
-                                            ingredient: currentIngredient,
-                                            category: currentCategoryName
-                                        });
+        // 1. label all of the items in the grocery
+        // list with their respective categories
+
+        let labeledList = [];
+
+        // Loop through all categories
+        for (var i = 0; i < categories.length; i++) {
+            let currentCategoryName = categories[i].CATEGORY;
+
+            // Loop through all ingredients in every category
+            for (var j = 0; j < categories[i].INGREDIENTS.length; j++) {
+                // Loop through list of grocery list items
+                for (var k = 0; k < list.length; k++) {
+                    // compare current item from grocery list and current
+                    // item from categories list
+
+                    let currentListItem = list[k].ingredient.toLowerCase();
+                    let currentCategoryItem =
+                        categories[i].INGREDIENTS[j].toLowerCase();
+
+                    if (currentCategoryItem === currentListItem) {
+                        // if we find a match, push the item and the category into the
+                        // labeled list
+                        currentListItem = list[k].ingredient;
+                        labeledList.push({
+                            ingredient: currentListItem,
+                            category: currentCategoryName,
+                        });
                     }
                 }
-
             }
         }
 
-        console.log(outputList);
-        console.log("output List ^");
-        return outputList;
-    }
+        // 2. Use labeled list to make category buckets with
+        // each list of items within that bucket
+
+        // initialize categorized list with the categories
+        // from ingredientCategories.json
+        let categorizedList = initializeCatList(categories);
+
+        // Sort items into their category buckets
+        for (var i = 0; i < labeledList.length; i++) {
+            for (var j = 0; j < categorizedList.length; j++) {
+                console.log(labeledList[i].category);
+                if (labeledList[i].category === categorizedList[j].category) {
+                    categorizedList[j].items.push(labeledList[i]);
+                }
+            }
+        }
+
+        // return final categorized list
+        return categorizedList;
+    };
 
     return (
-        <>{ ingredients !== undefined ? 
-        ingredients.map((ingredient) => {
-            return (
-                <li>
-                    {ingredient.ingredient}
-                </li>
-            )
-        }) : <></>
-        }</>
-    )
- }
-   
- export default PrintableList;
-   
+        <Grid>
+            {categorizedList !== [] ? (
+                categorizedList.map((category) => {
+                    return (
+                        <>
+                            {category.items.length !== 0 ? (
+                                <Grid.Column width={8}>
+                                    <h2>{category.category}</h2>
+                                    {category.items.map((item) => {
+                                        return <li>{item.ingredient}</li>;
+                                    })}
+                                </Grid.Column>
+                            ) : (
+                                <></>
+                            )}
+                        </>
+                    );
+                })
+            ) : (
+                <></>
+            )}
+        </Grid>
+    );
+};
+
+export default PrintableList;
--- react-electron/src/pages/PrintGroceries.js
+++ react-electron/src/pages/PrintGroceries.js
@@ -2,46 +2,66 @@
  * PrintGroceries.js
  * Page where user can print a list
  * of groceries for their own use
- * 
- * @date 7/28/2022
+ *
+ * @date 7/31/2022
  * @author Ashton Statz
  */
-import React from "react";
-import { Button } from "semantic-ui-react";
-import { NavButton, PrintableList } from "../components/"; 
-import Pdf from "react-to-pdf";
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { NavButton, PrintableList } from '../components/';
+import Pdf from 'react-to-pdf';
 
+// CurrentDate()
+// outputs nicely formatted date for user
+// in react component form
 function CurrentDate() {
     const d = new Date();
     let month = d.getMonth();
     let day = d.getDate();
     let year = d.getFullYear();
-    return <span>{month + "/" + day + "/" + year}</span>;
+    return <span>{month + '/' + day + '/' + year}</span>;
 }
- 
-const PrintGroceries = ({ingredients}) => {
 
-const ref = React.createRef();
-    
+const PrintGroceries = ({ ingredients }) => {
+    const ref = React.createRef();
+
     return (
-    <div>
-        <h1>grocer</h1>
-        <p>Printable Grocery List</p>
-        <Pdf targetRef={ref} filename="groceries.pdf">
-            {({ toPdf }) => <Button onClick={toPdf}>Download PDF</Button>}
-        </Pdf>
-        <div style={{paddingTop: "10px", paddingBottom: "10px"}}>
-            <div ref={ref} class="print-out">
-                <h2 style={{textAlign: "center", paddingTop: "15px"}}>Groceries <CurrentDate /></h2>
-                <div style={{padding: "10px", textAlign: "left"}}>
-                <PrintableList ingredients={ingredients} />
+        <div>
+            <h1>grocer</h1>
+            <p>Printable Grocery List</p>
+            <div style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+                <div ref={ref} class='print-out'>
+                    <div class='ui one column stackable center aligned page grid'>
+                        <div class='column twelve wide'>
+                            <h2
+                                style={{
+                                    textAlign: 'center',
+                                    paddingTop: '15px',
+                                }}
+                            >
+                                Groceries <CurrentDate />
+                            </h2>
+                            <div style={{ padding: '10px', textAlign: 'left' }}>
+                                <PrintableList ingredients={ingredients} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='ui horizontal list'>
+                <div class='item'>
+                    <Pdf targetRef={ref} filename='groceries.pdf'>
+                        {({ toPdf }) => (
+                            <Button onClick={toPdf}>Download PDF</Button>
+                        )}
+                    </Pdf>
+                </div>
+                <div class='item'>
+                    <NavButton text={'Return Home'} link={'/'} display={true} />
                 </div>
             </div>
         </div>
-        <NavButton text={"Return Home"} link={"/"} display={true}/>
-    </div>
     );
-
-}
+};
 
 export default PrintGroceries;
